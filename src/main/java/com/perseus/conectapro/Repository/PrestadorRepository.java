475ad8fe36c1a69de943623bb8214c84c6b92154
Danillo <danillo.lacerda77@gmail.com>
2025-04-12 17:38:58 -0300

    Correção nos DTOs e ajustes na lógica

--- src/main/java/com/perseus/conectapro/Repository/PrestadorRepository.java
+++ src/main/java/com/perseus/conectapro/Repository/PrestadorRepository.java
@@ -1,14 +1,15 @@
 package com.perseus.conectapro.Repository;
 
 import com.perseus.conectapro.Entity.Prestador;
-import com.perseus.conectapro.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PrestadorRepository extends JpaRepository<Prestador, Integer> {
 
-    List<Prestador> findByHabilidadesContaining(String habilidade);
+    List<Prestador> findByNome(String name);
     List<Prestador> findByEspecialidadesContaining(String especialidade);
 
 }
