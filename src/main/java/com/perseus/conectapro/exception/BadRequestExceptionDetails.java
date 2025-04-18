e0f52615c80aa319a9b7ed7b34454b6fe4821084
Danillo <danillo.lacerda77@gmail.com>
2025-04-18 18:31:15 -0300

    feat: adicionar validações personalizadas para atualização de usuários, prestadores e empresas
    - Implementadas validações de CPF, CNPJ, email, telefone, e outros campos nas atualizações.
    - Adicionadas mensagens de exceção personalizadas para erros de validação.
    - Criados métodos de validação específicos na camada de serviço para usuários, prestadores e empresas.
    - Alterações garantem maior clareza nas mensagens de erro durante o processo de atualização de dados.

---
+++ src/main/java/com/perseus/conectapro/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,9 @@
+package com.perseus.conectapro.exception;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails {
+}
---
+++ src/main/java/com/perseus/conectapro/handler/RestExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.perseus.conectapro.handler;
+
+import com.perseus.conectapro.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Log4j2
+public class RestExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+}
