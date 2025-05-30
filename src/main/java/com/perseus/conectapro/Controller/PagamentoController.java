c4d0fcf8acb6803b7e9d4c0d5e922944df39d8da
Danillo <danillo.lacerda77@gmail.com>
2025-05-30 20:13:33 -0300

    fix: corrigir mapeamento de pagamento

--- src/main/java/com/perseus/conectapro/Controller/PagamentoController.java
+++ src/main/java/com/perseus/conectapro/Controller/PagamentoController.java
@@ -6,8 +6,11 @@ import com.perseus.conectapro.Service.PagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/pagamento")
 public class PagamentoController {
     @Autowired
     private PagamentoService pagamentoService;
---
+++ src/main/java/com/perseus/conectapro/Controller/PerfilEmpresaController.java
@@ -0,0 +1,10 @@
+package com.perseus.conectapro.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/perfil/empresaCliente")
+public class PerfilEmpresaController {
+
+}
