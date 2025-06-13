a9c5f1c7495bdbaa342a82b31b2bd566a562b1b0
Danillo <danillo.lacerda77@gmail.com>
2025-06-13 14:08:37 -0300

    fix: corrigir bug no Pagamento

--- src/main/java/com/perseus/conectapro/Repository/PagamentoRepository.java
+++ src/main/java/com/perseus/conectapro/Repository/PagamentoRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
 
     boolean existsByIdServico(Servico servico);
     Pagamento findByIdPagamento(int idPagamento);
